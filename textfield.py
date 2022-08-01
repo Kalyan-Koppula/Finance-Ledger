1a94a4bfbe23dde046f0ca885117aed5d686db37
Kalyan-Koppula <kalyan.plmr@outlook.com>
2022-08-01 20:01:38 +0530

    Modify add_customer_page content

--- textfield.py
+++ textfield.py
@@ -1,59 +1,51 @@
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 from kivymd.app import MDApp
+from kivymd.uix.relativelayout import MDRelativeLayout
 
 KV = '''
-MDBoxLayout:
-    padding: "10dp"
-    spacing: "10dp"
-    orientation: "vertical"
+<ClickableTextFieldRound>:
+    size_hint_y: None
+    height: text_field.height
 
-    MDBoxLayout:
-        orientation: "horizontal"
-        padding: "10dp"
-        
-        MDIcon:
-            icon: "account"
-            size_hint_x: .25
-            pos_hint: {"center_y": .5,"center_x":1}
+    MDTextField:
+        id: text_field
+        hint_text: root.hint_text
+        text: root.text
+        password: True
+        icon_left: "key-variant"
 
-        MDTextField:
-            id: text_field_error
-            hint_text: "Name"
-            pos_hint: {"center_y": .5}
-            mode: "rectangle"
-            size_hint_x: 0.75
-            padding_right: "20dp"
-    
-    MDBoxLayout:
-        orientation: "horizontal"
-        padding: "10dp"
-        
-        MDIcon:
-            icon: "cash-multiple"
+    MDIconButton:
+        icon: "eye-off"
+        pos_hint: {"center_y": .5}
+        pos: text_field.width - self.width + dp(8), 0
+        theme_text_color: "Hint"
+        on_release:
+            self.icon = "eye" if self.icon == "eye-off" else "eye-off"
+            text_field.password = False if text_field.password is True else True
 
-        MDTextField:
-            id: text_field_error
-            hint_text: "Amount"
-            pos_hint: {"center_y": .5}
-            mode: "rectangle"
+
+MDScreen:
+
+    ClickableTextFieldRound:
+        size_hint_x: None
+        width: "300dp"
+        hint_text: "Password"
+        pos_hint: {"center_x": .5, "center_y": .5}
 '''
 
 
+class ClickableTextFieldRound(MDRelativeLayout):
+    text = StringProperty()
+    hint_text = StringProperty()
+    # Here specify the required parameters for MDTextFieldRound:
+    # [...]
+
+
 class Test(MDApp):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.screen = Builder.load_string(KV)
-
     def build(self):
-        self.screen.ids.text_field_error.bind(
-            on_text_validate=self.set_error_message,
-            on_focus=self.set_error_message,
-        )
-        return self.screen
-
-    def set_error_message(self, instance_textfield):
-        self.screen.ids.text_field_error.error = True
+        return Builder.load_string(KV)
 
 
 Test().run()
