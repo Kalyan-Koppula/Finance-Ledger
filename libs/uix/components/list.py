a744123599c537c2f5311529f41a65df403d1717
Kalyan-Koppula <kalyan.plmr@outlook.com>
2022-08-11 22:51:51 +0530

    Add popup

--- libs/uix/components/list.py
+++ libs/uix/components/list.py
@@ -2,6 +2,7 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivymd.uix.behaviors import RectangularRippleBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
+from libs.uix.components.profile_preview_dialog import ProfilePreview
 
 Builder.load_string(
     """
@@ -23,6 +24,8 @@ Builder.load_string(
         pos_hint: {'center_y': .5}
         spacing: dp(10)
         adaptive_height: True
+        on_touch_down:
+            if self.collide_point(*args[1].pos): root.open_dialog()
 
         MDLabel:
             id: name
@@ -62,8 +65,13 @@ class DailyCustomerListItem(RectangularRippleBehavior, MDBoxLayout):
 
     image = StringProperty()
 
+    installment = StringProperty()
+
     f_no = '123'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    def open_dialog(self):
+        ProfilePreview().fire(title=self.name, image=self.image)
 
